3d111298f439f61f22e695d28c09cdd6c1b63404
Kshitij Lauria <kslauria@gmail.com>
2016-03-06 10:40:03 -0500

    Refactor into separate pieces: touchmouse and netflix
    NetflixDevice emites the appropriate keys to the input system, while
    TouchmouseServer talks to the app and calls a generic handler with
    received data. A simple handler function combines the two to achieve
    the desired effect.

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
-class NetflixDevice(object):
+# to install uinput:
+# apt-get install libudev-dev
+# pip install python-uinput
+import uinput        
+class NetflixDevice(object):    
     def __init__(self):
         self._device = uinput.Device([uinput.KEY_SPACE,
                                       uinput.KEY_LEFT,
@@ -17,46 +21,57 @@ class NetflixDevice(object):
         self._device.emit_combo([uinput.KEY_LEFTSHIFT,
                                  uinput.KEY_RIGHT])
         
+import socket
+class TouchmouseServer(object):
+    touchmouse_port = 4026
+    def __init__(self):
+        self.listen_tcp()
+        self.publish_service()
+        self.listen_udp()
+        
+    def publish_service(self):
+        import os
+        if os.fork() == 0:
+            os.execl("/usr/bin/avahi-publish", "avahi-publish",
+                     "-s", socket.gethostname(),
+                     "_iTouch._tcp", str(self.touchmouse_port))
 
-touchmouse_port = 4026
+    def listen_tcp(self):
+        # The app looks for an open listening TCP socket on port 4026
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# to install uinput:
-# apt-get install libudev-dev
-# pip install python-uinput
+        # SO_REUSEADDR lets me run this code over and over without getting
+        # "Address already in use"
+        tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp_socket.bind(("", self.touchmouse_port))
+        tcp_socket.listen(1)
 
-import socket, uinput
+        self.tcp_socket = tcp_socket # make a reference to prevent it being garbage-collected
 
-def publish_service(port):
-    import os
-    if os.fork() == 0:
-        os.execl("/usr/bin/avahi-publish", "avahi-publish",
-                 "-s", socket.gethostname(),
-                 "_iTouch._tcp", str(port))
-publish_service(touchmouse_port)
+    def listen_udp(self):
+        # The actual data comes in on UDP on the same port
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket.bind(("", self.touchmouse_port))
 
-# The app looks for an open listening TCP socket on port 4026
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# SO_REUSEADDR lets me run this code over and over without getting
-# "Address already in use"
-tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcp_socket.bind(("", touchmouse_port))
-tcp_socket.listen(1)
+    def event_loop(self, handler):
+        while 1:
+            data, addr = self.udp_socket.recvfrom(8)
+            data = map(ord, data) # bytes->integer codes
+            handler(data)
+    
+if __name__ == "__main__":
+    netflix = NetflixDevice()
+    def netflix_handler(iphone_input):
+        data = iphone_input
+        if data[3] == 13: # keyboard code
+            qq = chr(data[7])
+            if  qq in "qwaszxedc":
+                netflix.rewind()
+            elif qq in "rfvtgbyhn":
+                netflix.toggle_pause()
+            elif qq in "ujmikolp":
+                netflix.fast_forward()
 
-# The actual data comes in on UDP on the same port
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind(("", touchmouse_port))
-
-netflix = NetflixDevice()
-while 1:
-    data, addr = udp_socket.recvfrom(8)
-    data = map(ord, data) # bytes->integer codes
-
-    if data[3] == 13: # keyboard code
-        qq = chr(data[7])
-        if  qq in "qwaszxedc":
-            netflix.rewind()
-        elif qq in "rfvtgbyhn":
-            netflix.toggle_pause()
-        elif qq in "ujmikolp":
-            netflix.fast_forward()
+    server = TouchmouseServer()
+    server.event_loop(netflix_handler)
