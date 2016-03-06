8ef8a0591885d1336c137a05e73562257bfc08f1
Kshitij Lauria <kslauria@gmail.com>
2016-03-06 02:17:48 -0500

    First useful version
    Use uinput library to emit SPACEBAR when any key is pressed on the
    phone. This pauses Netflix.

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
+class SpacebarDevice(object):
+    def __init__(self):
+        self._device = uinput.Device([uinput.KEY_SPACE])
+    def send_pause(self):
+        self._device.emit_click(uinput.KEY_SPACE)
+
 touchmouse_port = 4026
 
-import socket
+# to install uinput:
+# apt-get install libudev-dev
+# pip install python-uinput
+
+import socket, uinput
 
 # The app looks for an open listening TCP socket on port 4026
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,11 +26,13 @@ tcp_socket.listen(1)
 # The actual data comes in on UDP on the same port
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind(("", touchmouse_port))
-print "Waiting ..."
+
+spacebar = SpacebarDevice()
 while 1:
     data, addr = udp_socket.recvfrom(8)
     data = map(ord, data) # bytes->integer codes
 
     if data[3] == 13: # keyboard code
-        print chr(data[7])
+        if 'a' < chr(data[7]) < 'z':
+            spacebar.send_pause()
         
